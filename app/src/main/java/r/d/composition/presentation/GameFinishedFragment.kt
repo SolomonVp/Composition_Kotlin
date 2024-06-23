64f8803360152f6207a4082c69bfe9d02879aad5
SolomonVP <Lum7023@yandex.ru>
2024-06-23 23:01:10 +0300

    View Binding

--- app/src/main/java/r/d/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/r/d/composition/presentation/GameFinishedFragment.kt
@@ -6,13 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import r.d.composition.R
+import r.d.composition.databinding.FragmentChooseLevelBinding
+import r.d.composition.databinding.FragmentGameFinishedBinding
 
 class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+}
 }
--- app/src/main/java/r/d/composition/presentation/GameFragment.kt
+++ app/src/main/java/r/d/composition/presentation/GameFragment.kt
@@ -6,13 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import r.d.composition.R
+import r.d.composition.databinding.FragmentChooseLevelBinding
+import r.d.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
